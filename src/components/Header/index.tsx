606168636e3fd75e521f32b6ea343da7f1aabcbf
Neutron35 <steve.lieuron@gmail.com>
2024-10-15 10:12:31 +0200

    fix: better Header but not perfect yet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from '@/components/ui/navigation-menu';
 import {
   faAddressCard,
   faClipboard,
@@ -8,23 +10,14 @@ import {
   faRightToBracket,
   faSun,
   faUser,
+  faUserGear,
 } from '@fortawesome/free-solid-svg-icons';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Logo from '../../assets/neutron.png';
-import { memo } from 'react';
+import { Switch } from '../ui/switch';
+import logoImg from '../../assets/neutron.png';
 import { useAuth } from '@/lib/authProvider';
 import { useTheme } from '@/lib/themeProvider';
-
-// Memoize the FontAwesomeIcon component to prevent unnecessary re-renders
-const MemoizedFontAwesomeIcon = memo(FontAwesomeIcon);
-
-// Memoize the NavLink component to prevent unnecessary re-renders
-const MemoizedNavLink = memo(NavLink);
-
-// Define icons outside the component to ensure stable references
-const sunIcon = faSun;
-const moonIcon = faMoon;
 
 function Header() {
   const { darkTheme, setDarkTheme } = useTheme();
@@ -37,60 +30,72 @@ function Header() {
   }
 
   function handleLogout() {
-    setToken();
+    setToken(null);
     navigate('/', { replace: true });
   }
+
   return (
-    <header className="sticky top-0 w-full">
-      <nav className="py-1.5 px-8">
-        <ul className="w-4/6 mx-auto flex flex-row items-center justify-between text-xl">
-          <li className="ml-5 py-3.5 px-4">
-            <MemoizedNavLink to="/" className="navbar__logo">
-              <img src={Logo} className="h-12" alt="Logo" />
-            </MemoizedNavLink>
-          </li>
-          <li className="ml-5 py-3.5 px-4">
-            <MemoizedNavLink to="/" className="navbar__link">
-              <MemoizedFontAwesomeIcon icon={faHome} className="mr-2" />
+    <header className="sticky top-0 mx-auto flex items-center justify-center space-x-4 text-xl">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem className="mr-8 px-4 py-3.5">
+            <NavLink to="/">
+              <img src={logoImg} className="h-12" alt="Logo Neutron" />
+            </NavLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="px-4 py-3.5">
+            <NavLink to="/">
+              <FontAwesomeIcon icon={faHome} className="mr-2" />
               Accueil
-            </MemoizedNavLink>
-          </li>
-          <li className="ml-5 py-3.5 px-4">
-            <MemoizedNavLink to="/about" className="navbar__link">
-              <MemoizedFontAwesomeIcon icon={faUser} className="mr-2" />A propos
-            </MemoizedNavLink>
-          </li>
-          <li className="ml-5 py-3.5 px-4">
-            <MemoizedNavLink to="/projects" className="navbar__link">
-              <MemoizedFontAwesomeIcon icon={faClipboard} className="mr-2" />
+            </NavLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="px-4 py-3.5">
+            <NavLink to="/projects">
+              <FontAwesomeIcon icon={faClipboard} className="mr-2" />
               Projets
-            </MemoizedNavLink>
-          </li>
-          {token && (
-            <li className="ml-5 py-3.5 px-4">
-              <MemoizedNavLink to="/account" className="navbar__link">
-                <MemoizedFontAwesomeIcon icon={faAddressCard} className="mr-2" />
-              </MemoizedNavLink>
-            </li>
-          )}
-          <li className="ml-5 py-3.5 px-4">
-            {token ? (
-              <button onClick={handleLogout}>
-                <MemoizedFontAwesomeIcon icon={faRightFromBracket} className="mr-2" />
-              </button>
+            </NavLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="px-4 py-3.5">
+            <NavLink to="/about">
+              <FontAwesomeIcon icon={faAddressCard} className="mr-2" />A propos
+            </NavLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="px-4 py-3.5">
+            {!token ? (
+              <NavLink to="/login" className="ml-8">
+                <FontAwesomeIcon icon={faRightToBracket} />
+              </NavLink>
             ) : (
-              <MemoizedNavLink to="/login" className="navbar__link">
-                <MemoizedFontAwesomeIcon icon={faRightToBracket} className="mr-2" />
-              </MemoizedNavLink>
+              <div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild className="cursor-pointer px-4 py-3.5">
+                    <FontAwesomeIcon icon={faUser} className="ml-8 text-xl" />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="font-display">
+                    <DropdownMenuItem>
+                      <NavLink to="/account">
+                        <FontAwesomeIcon icon={faUserGear} className="pr-3" />
+                        Mon compte
+                      </NavLink>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <button onClick={handleLogout}>
+                        <FontAwesomeIcon icon={faRightFromBracket} className="pr-3" />
+                        Se déconnecter
+                      </button>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
             )}
-          </li>
-          <li className="ml-5 py-3.5 px-4">
-            <button onClick={handleToggle}>
-              <MemoizedFontAwesomeIcon icon={darkTheme ? moonIcon : sunIcon} className="mr-2" />
-            </button>
-          </li>
-        </ul>
-      </nav>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="absolute right-5">
+        <FontAwesomeIcon icon={faSun} />
+        <Switch onClick={handleToggle} checked={darkTheme} className="mx-2" />
+        <FontAwesomeIcon icon={faMoon} />
+      </div>
     </header>
   );
 }
